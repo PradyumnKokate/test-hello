96cfe4e93a0695ec4b0a34458e47ac37ea4878e7
PradyumnKokate <pradyumnakokate06@gmail.com>
2024-07-26 16:48:43 +0530

    update

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ import cv2 as cv2
 
 def run_yolo(image_url, confidence=0.15, iou=0.7):
     
-    yolo = YOLO(r'C:\Users\prady\Downloads\Streamlit\runs\detect\train\weights\best.pt')
+    yolo = YOLO(best.pt)
     results = yolo.predict(image_url,conf=confidence, iou=iou)
     res = results[0].plot()[:, :, [2,1,0]]
     return Image.fromarray(res)
