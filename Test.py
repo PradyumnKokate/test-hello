c47c42870840e257a133bbcc2e5e0abf6cd70453
PradyumnKokate <pradyumnakokate06@gmail.com>
2024-07-26 20:35:03 +0530

    update

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 def run_yolo(image_url, confidence=0.15, iou=0.7):
     
-    yolo = YOLO(best.pt)
+    yolo = YOLO("best.pt")
     results = yolo.predict(image_url,conf=confidence, iou=iou)
     res = results[0].plot()[:, :, [2,1,0]]
     return Image.fromarray(res)
